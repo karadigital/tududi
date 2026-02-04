4f2e13f0fdd1f179263a88b550eec701f481f3d0
William Tekimam <william.tekimam@gmail.com>
2026-02-04 15:17:48 +0700

    [ASID-858] Add Workspace entity, service, and Zustand store

---
+++ frontend/entities/Workspace.ts
@@ -0,0 +1,9 @@
+export interface Workspace {
+    id?: number;
+    uid?: string;
+    name: string;
+    creator?: number;
+    project_count?: number;
+    created_at?: string;
+    updated_at?: string;
+}
